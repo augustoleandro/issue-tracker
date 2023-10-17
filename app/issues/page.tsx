3bd5656b6c8ebc75fc137d6ce2e9cffb962ab178
Leandro Augusto <augustoleandro@gmail.com>
2023-10-17 10:04:34 -0300

    implement Issue detail page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import prisma from '@/prisma/client'
 import IssueStatusBadge from '../components/IssueStatusBadge'
 import delay from 'delay'
 import IssueActions from './IssueActions'
+import Link from 'next/link'
 
 async function IssuesPage() {
   const issues = await prisma.issue.findMany()
@@ -24,7 +25,9 @@ async function IssuesPage() {
           {issues.map(issue => (
             <Table.Row key={issue.id}>
               <Table.Cell>
-                {issue.title}
+                <Link href={`/issues/${issue.id}`}>
+                  {issue.title}
+                </Link>
                 <div className='block md:hidden'>
                   <IssueStatusBadge status={issue.status} />
                 </div>
