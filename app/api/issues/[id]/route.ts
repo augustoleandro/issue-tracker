9cee1ec35bb1f2e6459c102852d720943b599592
Leandro Augusto <augustoleandro@gmail.com>
2023-10-19 10:47:19 -0300

    build an api for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -32,3 +32,20 @@ export async function PATCH(request: NextRequest, { params } : { params: { id: s
   
   return NextResponse.json(updatedIssue, {status: 200})
 }
+
+export async function DELETE(request: NextRequest, { params } : { params: { id: string } }) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id)}
+  })
+
+  if(!issue) {
+    return NextResponse.json({ error: "Invalid issue"}, { status: 404 })
+  }
+
+  await prisma.issue.delete({
+    where: { id: parseInt(params.id)}
+  })
+
+  return NextResponse.json({}, { status: 200 })
+
+}
