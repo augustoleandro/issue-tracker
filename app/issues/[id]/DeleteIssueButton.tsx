6e7047c57e5394c5e18b5e3329fa9e26273d96e0
Leandro Augusto <augustoleandro@gmail.com>
2023-10-19 10:35:00 -0300

    show a confirmation dialog before delete issue

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,13 +1,33 @@
+"use client"
+
 import { TrashIcon } from '@radix-ui/react-icons'
-import { Button, Link } from '@radix-ui/themes'
+import { AlertDialog, AlertDialogTrigger, Button, Flex, Link } from '@radix-ui/themes'
 
 function DeleteIssueButton({ issueId }: { issueId: number }) {
   return (
-    <Link href={`/issues/${issueId}/edit`} className='flex justify-center'>
-      <Button color="red" className='w-full max-w-md'>
-        <TrashIcon />
-        Delete Issue
-      </Button>
+    <Link href="" className='flex justify-center'>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" className='w-full max-w-md'>
+            <TrashIcon />
+            Delete Issue
+          </Button>
+        </AlertDialog.Trigger>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+          <AlertDialog.Description>
+            Are you sure you want to delete this issue? This action cannot be undone.
+          </AlertDialog.Description>
+          <Flex mt="4" gap="3">
+            <AlertDialog.Cancel>
+              <Button variant='soft' color="gray">Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button color="red">Delete</Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
     </Link>
   )
 }
