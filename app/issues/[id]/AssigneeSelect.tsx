fcb07c4c34418410a5d4c4385dc8242e42545041
Leandro Augusto <augustoleandro@gmail.com>
2023-11-03 13:16:07 -0300

    refactor AssigneeSelect component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,46 +1,38 @@
 "use client"
 
+import { Skeleton } from '@/app/components'
 import { Issue, User } from '@prisma/client'
 import { Select } from '@radix-ui/themes'
 import { useQuery } from '@tanstack/react-query'
-import React, { useEffect, useState } from 'react'
-import { Skeleton } from '@/app/components'
 import toast, { Toaster } from 'react-hot-toast'
 
 function AssigneeSelect({ issue }: { issue: Issue }) {
-  const { data: users, error, isLoading } = useQuery<User[]>({
-    queryKey: ['users'],
-    queryFn: async () => {
-      const response = await fetch('/api/users');
-      const data = await response.json();
-      return data;
-    },
-    staleTime: 60 * 1000,
-    retry: 3,
-  })
+  const { data: users, error, isLoading } = useUsers()
 
   if (isLoading) return <Skeleton />
 
   if (error) return null
 
+  const assignToUser = async (userId: String) => {
+    const result = await fetch(`/api/issues/${issue.id}`, {
+      method: 'PATCH',
+      body: JSON.stringify({
+        assignedToUserId: userId === " " ? null : userId
+      })
+    });
+    console.log(result)
+    if (!result.ok) {
+      toast.error("Changes could not be saved")
+    } else {
+      toast.success("Changes saved")
+    }
+  }
+
   return (
     <>
       <Select.Root
         defaultValue={issue.assignedToUserId || " "}
-        onValueChange={async (userId) => {
-          const result = await fetch(`/api/issues/${issue.id}`, {
-            method: 'PATCH',
-            body: JSON.stringify({
-              assignedToUserId: userId === " " ? null : userId
-            })
-          });
-          console.log(result)
-          if (!result.ok) {
-            toast.error("Changes could not be saved")
-          } else {
-            toast.success("Changes saved")
-          }
-        }}>
+        onValueChange={assignToUser}>
         <Select.Trigger placeholder='Assign...' />
         <Select.Content>
           <Select.Group>
@@ -56,5 +48,16 @@ function AssigneeSelect({ issue }: { issue: Issue }) {
     </>
   )
 }
+
+const useUsers = () => useQuery<User[]>({
+  queryKey: ['users'],
+  queryFn: async () => {
+    const response = await fetch('/api/users');
+    const data = await response.json();
+    return data;
+  },
+  staleTime: 60 * 1000,
+  retry: 3,
+})
 
 export default AssigneeSelect
